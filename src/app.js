c288dd1d12eff18c5c39c443ce6180728c216792
[Justin Young] <[jyoung7223@yahoo.com]>
2021-02-22 21:27:29 -0700

    Removing unused crap

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
 const mongoose = require("mongoose");
-
-require('dotenv').config();
 
 const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
 app.use([
   express.urlencoded({ extended: true }), 
   express.json()
